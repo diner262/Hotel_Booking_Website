3caf78f4a74d9074fa42ea27e4934b280ba2b278
trinhduy444 <52000655@student.tdtu.edu.vn>
2023-03-19 21:17:12 +0700

    edit detail js

--- routes/detail.js
+++ routes/detail.js
@@ -4,3 +4,7 @@ var router = express.Router();
 router.get('/detail', function(req, res, next) {
     res.render('detail', { title: 'Detail' });
 });
+
+router.get('/detail/:id', function(req, res, next) {
+    res.render('detail', { title: 'Detail', id: req.params.id });
+});

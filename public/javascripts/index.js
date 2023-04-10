d146399adbb11bdaab5f00261d73a8aa710e3fd8
Trần Hoàng Quang Din <52000882@student.tdtu.edu.vn>
2023-04-10 21:17:58 +0700

    Fix architecture project

--- public/javascripts/index.js
+++
@@ -1,5 +0,0 @@
-function bookNow() {
-    // Replace this with your actual booking URL
-    window.location.href = 'https://www.example.com/book-now';
-};
-
---
+++ src/routes/detail.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var router = express.Router();
+
+router.get('/detail', function(req, res, next) {
+    res.render('detail', { title: 'Detail' });
+});
+
+router.get('/detail/:id', function(req, res, next) {
+    res.render('detail', { title: 'Detail', id: req.params.id });
+});
+
+module.exports = router;

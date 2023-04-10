48160ca1fc604cdbc1a14e08185ea32d93130d66
Trần Hoàng Quang Din <52000882@student.tdtu.edu.vn>
2023-04-10 20:35:00 +0700

    Change app.js to server.js

---
+++ server.js
@@ -0,0 +1,61 @@
+var createError = require('http-errors');
+var express = require('express');
+
+var hbs = require('express-handlebars')
+
+var path = require('path');
+require('dotenv').config({ path: path.resolve(__dirname, './.env') });
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+var detailRouter = require('./routes/detail');
+
+var port = process.env.PORT || 3000;
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, '/resources/views'));
+
+app.engine('hbs', hbs.engine({
+  defaultLayout: 'main',
+  extname: '.hbs',
+}))
+app.set('view engine', 'hbs');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, '/public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+app.use('/detail', detailRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('500');
+});
+
+
+
+
+app.listen(port, () => console.log(
+  `Express started on http://localhost:${port}; ` +
+  'Press Ctrl-C to terminate.'
+))
+
+module.exports = app;

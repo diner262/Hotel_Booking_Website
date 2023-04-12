b295f57587f913f831fb153a0fd1efe7c198e719
Trần Hoàng Quang Din <52000882@student.tdtu.edu.vn>
2023-04-12 23:43:51 +0700

    add manage layout

--- src/controllers/adminController.js
+++ src/controllers/adminController.js
@@ -6,9 +6,30 @@ class AdminController {
         res.render('admin/login', { layout: false });
     }
 
-    index(req, res, next) {
-        res.render('admin/index', {
+    home(req, res, next) {
+        res.render('admin/dashboard', {
             title: 'Dashboard',
+            layout: 'admin-main'
+        });
+    }
+
+    room_manage(req, res, next) {
+        res.render('admin/room_manage', {
+            title: 'Manage Room',
+            layout: 'admin-main'
+        });
+    }
+
+    order_manage(req, res, next) {
+        res.render('admin/order_manage', {
+            title: 'Manage Order',
+            layout: 'admin-main'
+        });
+    }
+
+    customer_manage(req, res, next) {
+        res.render('admin/customer_manage', {
+            title: 'Manage Customer',
             layout: 'admin-main'
         });
     }
--- src/routes/admin.routes.js
+++ src/routes/admin.routes.js
@@ -3,7 +3,10 @@ var router = express.Router();
 var adminController = require('../controllers/adminController');
 
 router.get('/login', adminController.login);
-router.get('/', adminController.index);
+router.get('/customer', adminController.customer_manage);
+router.get('/room', adminController.room_manage);
+router.get('/order', adminController.order_manage);
+router.get('/', adminController.home);
 
 
 module.exports = router;

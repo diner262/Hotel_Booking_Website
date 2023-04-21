4f3ae08824a9dbe99a7256365c65e8fee1d936ce
Trần Hoàng Quang Din <52000882@student.tdtu.edu.vn>
2023-04-21 22:44:49 +0700

    fix bookroom layout

--- src/server.js
+++ src/server.js
@@ -85,7 +85,7 @@ app.use('/admin/customers', express.static('public'));
 app.use('/admin/customers/update', express.static('public'));
 app.use('/admin/rooms/create', express.static('public'));
 app.use('/admin/rooms/update', express.static('public'));
-
+app.use('/bookroom', express.static('public'));
 
 // session middleware
 app.use(session({
